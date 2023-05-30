6809d0dad05d9f3deafe34ed7bfba790dfef33f6
preetham323 <preetham.kalangi@gmail.com>
2023-05-30 18:58:30 -0400

    modified

--- ecr.py
+++ ecr.py
@@ -1,7 +1,7 @@
 import boto3
 ecr_client = boto3.client('ecr')
 
-repository_name = "my-cloud-native-repo"
+repository_name = "kalangi-cloud-native-repo"
 response = ecr_client.create_repository(repositoryName=repository_name)
 repository_uri = reponse ['repository']['repositoryUri']
 print(repository_uri)
