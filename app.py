c5f374ac18081208d0dceffb8b54dffe347b05fc
preetham323 <preetham.kalangi@gmail.com>
2023-05-30 18:28:59 -0400

    changed the app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    cpu_metric = psutil.cpu_percent
-    mem_metic = psutil.virtual_memory().percent
+    cpu_percent = psutil.cpu_percent
+    mem_percent = psutil.virtual_memory().percent
     Message = None
     if cpu_metric > 80 or mem_metric > 80:
         Message = "High CPU or Memory detected, please saclue up"
 
-    return render_template("index.html", cpu_metric=cpu_metric, mem_metic=mem_metic, Message=Message)
+    return render_template("index.html", cpu_metric=cpu_percent, mem_percent=mem_percent, Message=Message)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')    
