169012ebeb8b2608acffcd3a342127815103c845
preetham323 <preetham.kalangi@gmail.com>
2023-05-30 21:02:41 -0400

    dd

--- eks.py
+++ eks.py
@@ -1,27 +1,30 @@
+#create deployment and service
 from kubernetes import client, config
 
+# Load Kubernetes configuration
 config.load_kube_config()
 
+# Create a Kubernetes API client
 api_client = client.ApiClient()
 
-#Define Deployment
+# Define the deployment
 deployment = client.V1Deployment(
-    metadata = client.V1ObjectMeta(name="my-flask-app"),
-    spec = client.V1DeploymentSpec(
+    metadata=client.V1ObjectMeta(name="my-flask-app"),
+    spec=client.V1DeploymentSpec(
         replicas=1,
         selector=client.V1LabelSelector(
             match_labels={"app": "my-flask-app"}
         ),
-        template = client.V1PodTemplateSpec(
-            metadata = client.V1ObjectMeta(
-                labels = {"app":"my-flask-app"}
+        template=client.V1PodTemplateSpec(
+            metadata=client.V1ObjectMeta(
+                labels={"app": "my-flask-app"}
             ),
-            spec = client.V1PodSpec(
-                containers = [
+            spec=client.V1PodSpec(
+                containers=[
                     client.V1Container(
-                        name = "my-flask-app",
-                        image = "346141603601.dkr.ecr.us-east-1.amazonaws.com/kalangi-cloud-native-repo:latest",
-                        ports = [client.V1ContainerPort(container_port=5000)]
+                        name="my-flask-container",
+                        image="346141603601.dkr.ecr.us-east-1.amazonaws.com/kalangi-cloud-native-repo:latest",
+                        ports=[client.V1ContainerPort(container_port=5000)]
                     )
                 ]
             )
@@ -29,25 +32,25 @@ deployment = client.V1Deployment(
     )
 )
 
-#create Deployment
+# Create the deployment
 api_instance = client.AppsV1Api(api_client)
 api_instance.create_namespaced_deployment(
-    namespace = "default",
-    body = deployment
+    namespace="default",
+    body=deployment
 )
 
-# Define service
+# Define the service
 service = client.V1Service(
-    metadata = client.V1ObjectMeta(name="my-flask-service"),
-    spec = client.V1ServiceSpec(
-        selector = {"app": "my-flask-app"},
-        ports = [client.V1ServicePort(port=5000)]
+    metadata=client.V1ObjectMeta(name="my-flask-service"),
+    spec=client.V1ServiceSpec(
+        selector={"app": "my-flask-app"},
+        ports=[client.V1ServicePort(port=5000)]
     )
 )
 
-#create the service
-api_instance = client.coreV1Api(api_client)
+# Create the service
+api_instance = client.CoreV1Api(api_client)
 api_instance.create_namespaced_service(
-    namespace = "default",
-    body = service
+    namespace="default",
+    body=service
 )
