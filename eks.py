a0fe909b15e1fdc8ec34303eacc3afdf1f3676de
preetham323 <preetham.kalangi@gmail.com>
2023-05-30 20:41:26 -0400

    modified eks.py

--- eks.py
+++ eks.py
@@ -7,27 +7,28 @@ api_client = client.Apiclient()
 #Define Deployment
 deployment = client.V1Deployment(
     metadata = client.V1ObjectMeta(name="my-flask-app"),
-    spec = client.V1DeploymentSpec(
-        replicas=1
-        selector = client.V1LabelSelector(
-            match_label = {"app": "my-flask-app"}
-        )
-    )
-    template = client.V1PodTemplateSpec(
-        metadata = client.V1ObjectMeta(
-            labels = {"app": "my-flask-app"}
-        )
-        spec = clent.V1PodSpec(
-            containers = [
-                client.V1Container(
-                    name = "my-flask-app"
-                    image = "346141603601.dkr.ecr.us-east-1.amazonaws.com/kalangi-cloud-native-repo:latest"
-                    ports = [client.V1ContainerPort(container_port=5000)]
-                )
-            ]
+    spec = client.V1DeploymentSoec(
+        replicas=1,
+        selector=client.V1LabelSelector(
+            match_labels={"app": "my-flask-app"}
+        ),
+        template = client.V1PodTemplateSpec(
+            metadata = client.V1ObjectMeta(
+                labels = {"app":"my-flask-app"}
+            ),
+            spec = client.V1PodSpec(
+                containers = [
+                    client.V1Container(
+                        name = "my-flask-app",
+                        image = "346141603601.dkr.ecr.us-east-1.amazonaws.com/kalangi-cloud-native-repo:latest",
+                        ports = client.V1ContainerPort(container_port=5000)
+                    )
+                ]
+            )
         )
     )
 )
+
 #create Deployment
 api_instance = client.AppsV1Api(api_client)
 api_instance.create_namespaced_deployment(
